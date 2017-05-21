ff9a265c3fa05e957c624e52a1d8348de895f61c
perzuz <harrisscott@outlook.com>
2017-05-21 21:14:29 +0100

    Add: Charting library and shell for bar chart in the display fragment

--- SortingAlgorithms/app/src/main/java/kbb/sortingalgorithms/app/Views/DisplayAlgorithmFragment.java
+++ SortingAlgorithms/app/src/main/java/kbb/sortingalgorithms/app/Views/DisplayAlgorithmFragment.java
@@ -9,7 +9,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import kbb.sortingalgorithms.R;
 import kbb.sortingalgorithms.app.Presenters.DisplayAlgorithmPresenter;
@@ -50,5 +55,24 @@ public class DisplayAlgorithmFragment extends android.support.v4.app.Fragment im
     public void setTitle(String title) {
         binding.title.setPaintFlags(binding.title.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
         binding.title.setText(title);
+
+        ArrayList<Integer> integers = new ArrayList<Integer>();
+        integers.add(0,24);
+        integers.add(1,12);
+        integers.add(2,36);
+        integers.add(3,65);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+
+        for (Integer data : integers) {
+            entries.add(new BarEntry(data,1));
+        }
+
+        BarEntry entry = new BarEntry(10,4);
+        BarData barData = new BarData();
+        barData.addEntry(entry,1);
+
+        binding.chart.setData(barData);
+        binding.chart.invalidate();
     }
 }
